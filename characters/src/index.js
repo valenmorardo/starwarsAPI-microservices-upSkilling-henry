5fe244e27ff477a1bd0472432964cfab273c3517
Valentin <valentin.morardo@gmail.com>
2023-09-18 17:49:27 -0300

    characters service files refactor

--- characters/src/index.js
+++ characters/src/index.js
@@ -15,8 +15,6 @@ import startServer from "./config/functions/startServer.js";
 const runApp = async () => {
 	try {
 		await startServer();
-		console.log(`~~CHARACTERS on port --> ${process.env.PORT}`)
-
 	} catch (err) {
 		console.log('ERROR!!');
 		console.log(err)
