b69b0e7403d6fcf99107778a42493fc178935c1b
Valentin <valentin.morardo@gmail.com>
2023-09-18 19:56:42 -0300

    error handler added

---
+++ database/src/controllers/errorHandler.js
@@ -0,0 +1,10 @@
+export const errorHandler = (error, _req, res, _next) => {
+  const status = error.response ? error.response.status : error.status;
+  console.log(error);
+
+  return res.status(status || 500).send({
+    message: error.message,
+    error_data: error.error_data,
+    aditional_data: error.aditional_data,
+  });
+};
