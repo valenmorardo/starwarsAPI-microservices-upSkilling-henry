3e75722aba3556941366104f4ca2a0e739c8bbd2
Valentin <valentin.morardo@gmail.com>
2023-09-20 20:06:36 -0300

    added controller new register to routes

--- database/src/controllers/index.controllers.js
+++ database/src/controllers/index.controllers.js
@@ -2,10 +2,12 @@ import catchedErrorAsync from "../utils/catchedErrorAsync.js";
 
 import listData from "./listData/listData.controller.js";
 import getDataById from "./getDataByID/getDataById.controller.js";
+import insertNewRegister from "./insertNewRegister/insertNewRegister.js";
 
 const controller = {
   listData: catchedErrorAsync(listData),
   getDataById: catchedErrorAsync(getDataById),
+  insertNewRegister: catchedErrorAsync(insertNewRegister),
 };
 
 export default controller;
