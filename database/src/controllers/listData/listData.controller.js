b18b6b6eee3d6d61cca3d4758694ae36a352ce35
Valentin <valentin.morardo@gmail.com>
2023-09-20 21:48:12 -0300

    controller updated

--- database/src/controllers/listData/listData.controller.js
+++ database/src/controllers/listData/listData.controller.js
@@ -6,9 +6,6 @@ const listData = async (req, res, _next) => {
   const { model } = req.params;
 
   const data = await store[model].list();
-  if (!data)
-    throw new CustomError(`Error to find data of ${model}`, 404, `Data of ${model} not founded`);
-
   return customResponse(res, 200, data);
 };
 
