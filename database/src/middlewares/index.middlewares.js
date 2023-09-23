a52e61113611abdeeb3d9fd976a3d289818f4008
Valentin <valentin.morardo@gmail.com>
2023-09-23 00:03:51 -0300

    indexm iddlewares updated

--- database/src/middlewares/index.middlewares.js
+++ database/src/middlewares/index.middlewares.js
@@ -1,9 +1,11 @@
 import validateModelParams from "./validateModelParams.js";
 import validateIdParams from "./validateIdParams.js";
+import validateDataBody from "./validateDataBody.js";
 
 const middleware = {
   validateModelParams,
   validateIdParams,
+  validateDataBody,
 };
 
 export default middleware;
