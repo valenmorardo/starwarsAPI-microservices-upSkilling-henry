ee7772136b479153e323eeca9ede3b49307bf088
Valentin <valentin.morardo@gmail.com>
2023-09-22 23:58:49 -0300

    index export all validations added

--- database/src/utils/validations/index.validations.js
+++ database/src/utils/validations/index.validations.js
@@ -1,7 +1,19 @@
-import isNumber from "./isNumber.js";
+import isAlphanumeric from "./isAlphanumeric.js";
+import isAlpha from "./isAlpha.js";
+import isArrayOfNumbers from "./isArrayOfNumbers.js";
+
+import isEmpty from "./isEmpty.js";
+import isNumeric from "./isNumeric.js";
+import isDate from "./isDate.js";
 
 const validations = {
-  isNumber,
+  isAlpha,
+  isAlphanumeric,
+  isArrayOfNumbers,
+  
+  isEmpty,
+  isNumeric,
+  isDate,
 };
 
 export default validations;
