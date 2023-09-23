ba69b70c6ee15d176d881e81fc64b4e2756e7c2a
Valentin <valentin.morardo@gmail.com>
2023-09-23 18:33:03 -0300

    ahora se hace el get al microservicio de la base de datos para obtener la informacion

--- films/src/controller/films/getFilms.controller.js
+++ films/src/controller/films/getFilms.controller.js
@@ -3,11 +3,13 @@ import CustomError from "../../utils/CustomError.js";
 
 import customResponse from "../../utils/customResponse.js";
 
-const getFilms = async (req, res) => {
-  const data = Films.list();
+import axios from 'axios'
 
-  if (!data) throw new CustomError("error", 400, "error to find films.");
-  response(res, 200, data);
+const getFilms = async (req, res) => {
+  const response = await axios.get("http://database:3005/Film");
+
+  if (!response.data) throw new CustomError("error", 400, "error to find films.");
+  customResponse(res, 200, response.data);
 };
 
 export default getFilms;
