1a5b48e098aab94d862b2ba9ad995727a34ffa52
Valentin <valentin.morardo@gmail.com>
2023-09-23 00:01:21 -0300

    updated controller insert new register

--- database/src/controllers/insertNewRegister/insertNewRegister.js
+++ database/src/controllers/insertNewRegister/insertNewRegister.js
@@ -1,13 +1,13 @@
 import store from "../../database/index.js";
 import customResponse from "../../utils/customResponse.js";
+
 import CustomError from "../../utils/customError.js";
+import validations from "../../utils/validations/index.validations.js";
 
 const insertNewRegister = async (req, res, _next) => {
   const { model } = req.params;
   const dataNewRegister = req.body;
-
   const newRegister = await store[model].insert(dataNewRegister);
-
   customResponse(res, 201, newRegister);
 };
 
