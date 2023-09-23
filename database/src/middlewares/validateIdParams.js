547cadaf0f6d2ffe8678b8b20179182b30bcac75
Valentin <valentin.morardo@gmail.com>
2023-09-23 00:00:42 -0300

    validateId params middleware added

--- database/src/middlewares/validateIdParams.js
+++ database/src/middlewares/validateIdParams.js
@@ -4,7 +4,7 @@ import validations from "../utils/validations/index.validations.js";
 const validateIdParams = (req, res, next) => {
   const { id } = req.params;
 
-  if (validations.isNumber(id)) {
+  if (validations.isNumeric(id)) {
     return next();
   }
   throw new CustomError("Error at ID", 400, "The ID must be a INT number");
