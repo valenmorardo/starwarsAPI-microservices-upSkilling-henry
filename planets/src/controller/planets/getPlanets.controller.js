aa186f73417dc721f7792ca3b000d6438c0f8ffd
Valentin <valentin.morardo@gmail.com>
2023-09-29 13:25:23 -0300

    import file fix error

--- planets/src/controller/planets/getPlanets.controller.js
+++ planets/src/controller/planets/getPlanets.controller.js
@@ -1,6 +1,6 @@
 import { Planets } from "../../data/index.js";
 import customResponse from "../../utils/customResponse.js";
-import CustomError from "../../utils/CustomError.js";
+import CustomError from "../../utils/customError.js";
 import axios from 'axios'
 
 const getPlanets = async (req, res) => {
