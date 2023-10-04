12e7c4927f23d518e4b282ee3b077ec0f6bae6a5
Valentin <valentin.morardo@gmail.com>
2023-10-04 20:52:53 -0300

    get planets controller updated

--- planets/src/controller/planets/getPlanets.controller.js
+++ planets/src/controller/planets/getPlanets.controller.js
@@ -1,4 +1,4 @@
-import { Planets } from "../../data/index.js";
+
 import customResponse from "../../utils/customResponse.js";
 import CustomError from "../../utils/customError.js";
 import axios from 'axios'
