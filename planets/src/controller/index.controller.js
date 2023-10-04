e3503d6b1d8ac507c9bd65520b7253be2c26ecc4
Valentin <valentin.morardo@gmail.com>
2023-10-04 20:54:01 -0300

    index controller updated with all controllers

--- planets/src/controller/index.controller.js
+++ planets/src/controller/index.controller.js
@@ -1,9 +1,11 @@
 import catchedErrorAsync from "../utils/catchedErrorAsync.js";
 
 import getPlanets from "./planets/getPlanets.controller.js";
+import postNewPlanet from "./planets/postNewPlanet.controller.js";
 
 const controller = {
   getPlanets: catchedErrorAsync(getPlanets),
+  postNewPlanet: catchedErrorAsync(postNewPlanet)
 };
 
 export default controller;
