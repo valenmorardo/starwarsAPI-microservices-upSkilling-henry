49d3fbaffb7dff90e5d0c611d3db98d91ac8023b
Valentin <valentin.morardo@gmail.com>
2023-10-04 20:48:22 -0300

    error handler updated

--- planets/src/controller/errorHandler.js
+++ planets/src/controller/errorHandler.js
@@ -1,10 +1,9 @@
 const errorHandler = (error, _req, res, _next) => {
   const status = error.response ? error.response.status : error.status;
-  console.log(error);
 
   return res.status(status || 500).send({
     message: error.message,
-    error_data: error.error_data,
+    error_data: error.error_data || error.response.data,
     aditional_data: error.aditional_data,
   });
 };
