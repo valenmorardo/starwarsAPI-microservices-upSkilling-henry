ac99f7f4e13d0468efcb09d1d4e410ccda051eaf
Valentin <valentin.morardo@gmail.com>
2023-10-05 16:44:33 -0300

    postNew film controller exported

--- films/src/controller/index.controller.js
+++ films/src/controller/index.controller.js
@@ -1,9 +1,11 @@
 import getFilms from "./films/getFilms.controller.js";
+import postNewFilm from "./films/postNewFilm.controller.js";
 
 import catchedErrorAsync from "../utils/catchedErrorAsync.js";
 
 const controller = {
   getFilms: catchedErrorAsync(getFilms),
+  postNewFilm: catchedErrorAsync(postNewFilm)
 };
 
 export default controller;
