4928b8e5044a6974d611593b19eb5ea1ea463dad
Valentin <valentin.morardo@gmail.com>
2023-10-05 16:42:36 -0300

    proxys definited

--- gateway/src/config/server/server.cfg.js
+++ gateway/src/config/server/server.cfg.js
@@ -4,17 +4,14 @@ import env from "../varEnvironments.js";
 import express from "express";
 import morgan from "morgan";
 
-import { createProxyMiddleware } from "http-proxy-middleware";
-
+import proxys from "../../proxys/index.js";
 
 const server = express();
 
 server.use(morgan("dev"));
-
 server.set("port", env.PORT);
-server.use(bodyParser.json());
-server.use(express.json());
 server.use(cookieParser());
+
 // cargamos body parser que es un middleware para analizar cuerpos atravez de la url
 server.use(bodyParser.urlencoded({ extended: false }));
 // activamos el CORS para permitir las peticions AJAX y HTTP desde el front
@@ -23,7 +20,7 @@ server.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Access-Control-Allow-Headers, Authorization, X-API-KEY, Origin, X-Requested-With, Content-type, Accept, Access-Control-Request-Method, Access-Control-Allow-Request-Method, Access-Control-Request-Headers,  Access-Control-Allow-Origin"
-  );
+    );
   res.setHeader("Access-Control-Allow-Headers", "*");
   res.setHeader("Access-Control-Request-Headers", "*");
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -37,33 +34,20 @@ server.use((req, res, next) => {
 });
 
 // PROXYS MIDDLEWARES
-server.use(
-  "/characters",
-  createProxyMiddleware({
-    target: "http://characters:3002",
-    changeOrigin: true,
-  })
-);
 
-server.use(
-  "/films",
-  createProxyMiddleware({
-    target: "http://films:3003",
-    changeOrigin: true,
-  })
-);
+server.get("/", (req, res) => {
+  res.status(200).send("This is the GATEWAY");
+});
 
-server.use(
-  "/planets",
-  createProxyMiddleware({
-    target: "http://planets:3004",
-    changeOrigin: true,
-  })
-);
+server.use("/planets", proxys.proxyPlanets);
+server.use("/films", proxys.proxyFilms);
+server.use("/characters", proxys.proxyCharacters);
+
+server.use(bodyParser.json());
+server.use(express.json());
 
 server.use("*", (_req, _res) => {
   throw new Error("URL Not found");
 });
-
 
 export default server;
