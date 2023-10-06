752cb059a996eeff2ab34235dfb1c0ba3a888e76
Valentin <valentin.morardo@gmail.com>
2023-10-06 20:35:49 -0300

    post new character exported

--- characters/src/controller/index.controller.js
+++ characters/src/controller/index.controller.js
@@ -1,8 +1,9 @@
 import { getCharacters } from "./characters/getCharacters.controller.js";
-
+import postNewCharacter from "./characters/postNewCharacter.controller.js";
 import catchedErrorAsync from "../utils/catchedErrorAsync.js";
 
 const controller = {
   getCharacters: catchedErrorAsync(getCharacters),
+  postNewCharacter: catchedErrorAsync(postNewCharacter),
 };
 export default controller;
